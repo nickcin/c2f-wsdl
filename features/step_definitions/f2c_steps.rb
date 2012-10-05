9e55b8703effb9d53355d914c3122a9f261a6f95
Nick Lyons <telagraphic@gmail.com>
2012-10-05 14:38:44 -0400

    delete comments

--- features/step_definitions/f2c_steps.rb
+++ features/step_definitions/f2c_steps.rb
@@ -7,17 +7,11 @@ Given /^I want a new wsdl client$/ do
 end
 
 When /^the user sends a fahrenheit request of (\d+)$/ do |f|
-  #puts fahrenheit
   @response = @client.request :tem, :fahrenheit_to_celsius, body: { "Fahrenheit" => f }
-  puts @response.to_hash
 end
-#@client.request :tem, :celsius_to_fahrenheit, body: { "Celsius" => "100" }
-
-
 
 Then /^the user should get a celsius response of (\d+)$/ do |arg1|
-  #puts @response.to_hash
-  #expect(arg1).to eq(@response)
+  expect(arg1).to eq(@response.to_hash[:fahrenheit_to_celsius_response][:fahrenheit_to_celsius_result])
 end
 
 
